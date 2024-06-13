a45bcaf256a56c46e3995f4b5e2bfb16b7fce2d0
Salladen <Salladen@users.noreply.github.com>
2024-06-13 16:54:17 +0200

    Based data serialization on Console output. Upgraded name Regex pattern.

--- Assignment2_Console/Program.cs
+++ Assignment2_Console/Program.cs
@@ -11,6 +11,29 @@ namespace Assignment2_Console;
 
 static class Program
 {
+
+    public static class RegexConstants
+    {
+        /* ** Shared strings ** */
+        // Patterns of: (Capital letter followed by lowercase letters) concatenated by a space or hyphen
+        private const string namePatBody = @"[A-ZÄÅÖ](?:[a-zäöå]*(?:[ -][A-ZÄÅÖ])*)*";
+        // 10 or 12 digits
+        private const string idPatBody = @"\d{10}|d{12}";
+        // 1 or more digits
+        private const string salesPatBody = @"\d+";
+        /* ** Shared strings ** */
+        
+        // Data field patterns
+        public static Regex namePattern = new Regex($"^{namePatBody}$", RegexOptions.CultureInvariant);
+        public static Regex idPattern = new Regex($"^{idPatBody}$", RegexOptions.CultureInvariant);
+        public static Regex salesPattern = new Regex($"^{salesPatBody}$", RegexOptions.CultureInvariant);
+        
+        // For parsing the output log file (please don't make me parse files like this)
+        public static Regex logPattern = new Regex($@"({namePatBody})\ +({idPatBody})\ +({namePatBody})\ +({salesPatBody})\ +", RegexOptions.CultureInvariant);
+        public static Regex logHeaderPattern = new Regex(@"(\w+)\ +(\w+)\ +(\w+)\ +(\w+)\ +", RegexOptions.CultureInvariant);
+    }
+    
+    
     class PriorityQueue<T>(Func<T, int> priorityFunction, bool ascending = true, int capacity = 1) : IEnumerable<T>
     {
         private readonly List<T> _list = new (capacity);
@@ -35,16 +58,20 @@ static class Program
         {
             // Assume a search space of the entire list
             int min = 0;
-            int max = _list.Count - 1;
+            int max = _list.Count;
+            
+            
+            // Modified priority function to account for ascending/descending order
+            Func<T, int> modPriorityFunction = x => ascending ? priorityFunction(x) : -priorityFunction(x);
             
             // Sign of priority is flipped in case of descending order
-            int priority = priorityFunction(value) * (ascending ? 1 : -1);
+            int priority = modPriorityFunction(value);
             
             // Halve the search space until we find the index to insert the value at
             while (min < max)
             {
                 int mid = (min + max) / 2;
-                int midPriority = priorityFunction(_list[mid]);
+                int midPriority = modPriorityFunction(_list[mid]);
 
                 if (midPriority < priority) max = mid;
                 else min = mid + 1;
@@ -90,8 +117,8 @@ static class Program
             return GetEnumerator();
         }
     }
-    
-    readonly struct SalesPerson(string name, string id, string district, int sales)
+
+    internal readonly struct SalesPerson(string name, string id, string district, int sales)
     {
         public string Name { get; init; } = name;
         public string Id { get; init; } = id;
@@ -167,86 +194,174 @@ static class Program
         }
     }
     
+    struct Tier
+    {
+        public Range Range { get; init; }
+        public List<SalesPerson> SalesPeople { get; init; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not Tier tier) return false;
+            return Range.Equals(tier.Range);
+        }
+        
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Range);
+        }
+    }
+    
     static void PrintSalesData(string path)
     {
-        int[] salesTiers = { 50, 100, 200 };
+        int[] salesTiers = { 50, 100, 200};
         // Read all lines from 'sales_data' file
-        string[] lines = File.ReadAllLines(path);
-        string[] headers = lines[0].Split(',');
+        FileStream fs = File.Open(path, FileMode.OpenOrCreate, FileAccess.Read);
+        StreamReader sr = new StreamReader(fs);
+        string[] lines = sr.ReadToEnd().Split('\n');
+        sr.Close();
+        fs.Close();
+        
+        // CSV logic
+        // string[] headers = lines[0].Split(',');
+        
+        // Needlessly complex regex logic
+        // Match the header line
+        Match headerMatch = RegexConstants.logHeaderPattern.Match(lines[0]);
+        if (!headerMatch.Success) return;
+        string[] headers = new string[headerMatch.Groups.Count - 1];
+        for (int i = 1; i < headerMatch.Groups.Count; i++)
+        {
+            headers[i-1] = headerMatch.Groups[i].Value;
+        }
+        
         ColumnWriter columnWriter = new ColumnWriter(headers.Length);
         foreach (string header in headers) columnWriter.Add(header);
         PriorityQueue<SalesPerson> salesPeoplePq = new Program.PriorityQueue<SalesPerson>(salesPerson => salesPerson.Sales, true, lines.Length - 1);
         
-        // Loop through all lines in 'sales_data' file
+        /*
+        // Loop through all lines in 'sales_data' file and parse it | CSV format
         for (int i = 1; i < lines.Length; i++)
         {
             string[] values = lines[i].Split(',');
             salesPeoplePq.Enqueue(new SalesPerson(values[0], values[1], values[2], int.Parse(values[3])));
         }
+        */
+        
+        // Loop through all lines in 'sales_data' file and parse it | stdout log regex matching format????
+        // ... who thought this was a good idea?
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string line = lines[i];
+            Match match = RegexConstants.logPattern.Match(line);
+            if (!match.Success) continue;
+            string name = match.Groups[1].Value;
+            string id = match.Groups[2].Value;
+            string district = match.Groups[3].Value;
+            int sales = int.Parse(match.Groups[4].Value);
+            salesPeoplePq.Enqueue(new SalesPerson(name, id, district, sales));
+        }
         
         if (salesPeoplePq.Count == 0) return;
         
-        // Use binary search to find the boundary indices of the sales tiers
-        int[] salesTiersIndices = new int[salesTiers.Length];
-        for (int i = 0; i < salesTiers.Length; i++)
-        {
-            int salesTier = salesTiers[i];
-            int salesTierIndex = salesPeoplePq.Where(new SalesPerson("", "", "", salesTier));
-            
-            salesTiersIndices[i] = (salesPeoplePq.Count - 1) - salesTierIndex;
-        }
-        
+        SalesPerson[] salesPeople = new SalesPerson[salesPeoplePq.Count];
         int idx = 0;
         foreach (SalesPerson salesPerson in salesPeoplePq)
         {
-            columnWriter.Add($"|{salesPerson.Name}");
-            columnWriter.Add($"|{salesPerson.Id}");
-            columnWriter.Add($"|{salesPerson.District}");
-            columnWriter.Add($"|{salesPerson.Sales.ToString()}");
-
-            if (salesPerson.Name == "Test6")
-            {
-                Console.WriteLine("Test6");
-            }
+            salesPeople[idx++] = salesPerson;
+        }
+        
+        Dictionary<SalesPerson, Tier> salesPersonTiers = new ();
+        List<Tier> tiers = new ();
+        for (int t = 1; t <= salesTiers.Length + 1; t++)
+        {
+            int low = (t > 1) ? salesTiers[t - 2] : 0;
+            int high = (t != salesTiers.Length + 1) ? salesTiers[t - 1] : int.MaxValue;
             
-            // Use the boundary indices to determine if we should print a sales tier
-            if (salesTiersIndices.Contains(idx) ||
-                (salesPeoplePq.Count == 0 && salesPerson.Sales >= salesTiers.Last()))
+            // Create a range object to print the range of the tier
+            Range range = new Range(low, high);
+            tiers.Add(new Tier
             {
-                // Determine the tier level
-                int tierLevel = salesTiersIndices.ToList().IndexOf(idx) + 1;
-                // If the tier level is 0, then we are in the highest tier due to how we calculate the indices
-                if (tierLevel == 0) tierLevel = salesTiers.Length + 1;
-                
-                // Determine the range of the tier, we have custom logic for the lowest and highest tiers
-                int low = (tierLevel > 1) ? salesTiers[tierLevel - 2] : 0;
-                int high = (tierLevel != salesTiersIndices.Length + 1) ? salesTiers[tierLevel - 1] : salesPerson.Sales;
+                Range = range,
+                SalesPeople = new List<SalesPerson>()
+            });
+        }
+        foreach (SalesPerson salesPerson in salesPeople)
+        {
+            foreach (Tier tier in tiers)
+            {
+                if (tier.Range.Start.Value <= salesPerson.Sales && salesPerson.Sales < tier.Range.End.Value)
+                {
+                    tier.SalesPeople.Add(salesPerson);
+                    salesPersonTiers[salesPerson] = tier;
+                    break;
+                }
+            }
+        }
+        
+        for (int h = 0; h < salesPeople.Length; h++)
+        {
+            SalesPerson salesPerson = salesPeople[h];
+            
+            columnWriter.Add($"{salesPerson.Name}");
+            columnWriter.Add($"{salesPerson.Id}");
+            columnWriter.Add($"{salesPerson.District}");
+            columnWriter.Add($"{salesPerson.Sales.ToString()}");
+            
+            Tier? prevTier = (h + 1 < salesPeople.Length) ? salesPersonTiers[salesPeople[h + 1]] : salesPersonTiers[salesPeople[h]];
+            if (prevTier == null)
+            {
+                continue;
+            }
+
+            if (!salesPersonTiers[salesPerson].Equals(prevTier)  || h == salesPeople.Length - 1)
+            {
+                Tier tier = salesPersonTiers[salesPerson];
+                int tierLevel = tiers.IndexOf(salesPersonTiers[salesPerson]) + 1;
                 bool highestTier = tierLevel > salesTiers.Length;
-                
-                // Create a range object to print the range of the tier
-                Range range = new Range(low, high);
                 // The highest tier has no upper bound and would therefore be inaccurate to present as a range
                 if (!highestTier)
                 {
-                    columnWriter.Add($"Sales Tier: {tierLevel} ({range.Start} - {range.End})");
+                    columnWriter.Add($"{tier.SalesPeople.Count} säljare har nått nivå {tierLevel}: {tier.Range.Start}-{tier.Range.End} artiklar");
                 }
                 else
                 {
                     // According to previous comments, Start+ is used to indicate lack of upper bound
-                    columnWriter.Add($"Sales Tier: {tierLevel} ({range.Start}+)");
+                    columnWriter.Add($"{tier.SalesPeople.Count} säljare har nått {tierLevel}: över {tier.Range.Start.Value-1} artiklar");
                 }
                 for (int i = 0; i < columnWriter.ColumnCount - 1; i++) columnWriter.Add("");
                 
                 // Add a separator
-                columnWriter.Add("".PadLeft(20, '-'));
+                columnWriter.Add("".PadLeft(0, '_'));
                 for (int i = 0; i < columnWriter.ColumnCount - 1; i++) columnWriter.Add("");
             }
-
-            idx++;
         }
         
         // The column writer has all the strings it can figure out all needed padding and spacing for each column
         columnWriter.Write();
+    }
+    
+    static void DumpSalesDataLog(string path)
+    {
+        // Redirect the console output to a file and print the sales data to 'sales_data.log'
+        TextWriter consoleOut = Console.Out;
+        
+        FileStream fs = File.Open("sales_data2.temp", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        StreamWriter sw = new StreamWriter(fs);
+        StreamReader sr = new StreamReader(fs);
+        
+        // Empty the file
+        fs.SetLength(0);
+        Console.SetOut(sw);
+        PrintSalesData(path);
+        fs.Seek(0, SeekOrigin.Begin);
+        consoleOut.Write(sr.ReadToEnd());
+        sw.Close();
+        fs.Close();
+        
+        // Overwrite 'sales_data2' with 'sales_data2.temp'
+        File.Delete(path);
+        File.Move("sales_data2.temp", path);
+        Console.SetOut(consoleOut);
     }
     
     // Simple method to write a message in a specific color
@@ -267,13 +382,13 @@ static class Program
         Console.WriteLine("".PadLeft(20, '='));
         Console.WriteLine("SalesSystem");
         Console.WriteLine($"Culture: {culture.Name}");
+        Console.WriteLine("".PadLeft(20, '='));
+        Console.Write("\n\n");
         
-        // Set underline (ansi escape sequence)
-        WriteLineInColor("Säljarstatistik:", ConsoleColor.Cyan);
-        WriteLineInColor("".PadLeft(20, '-'), ConsoleColor.Cyan);
-        PrintSalesData("sales_data");
-        WriteLineInColor("".PadLeft(20, '-'), ConsoleColor.Cyan);
+        // PrintSalesData("sales_data2");
+        DumpSalesDataLog("sales_data2");
         
+        Console.Write("\n\n");
         Console.WriteLine("".PadLeft(20, '='));
     }
     
@@ -294,11 +409,10 @@ static class Program
         // we're not using them extensively enough to warrant compilation
         
         // Capital letter followed by a word or words separated by a space
-        // This will match trailing spaces as well
-        Regex namePattern = new Regex(@"^(([A-Z])\w* {0,1})+$", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
-        // 10-12 digits
-        Regex idPattern = new Regex(@"^\d{10,12}$", RegexOptions.CultureInvariant);
-        Regex salesPattern = new Regex(@"^\d+$", RegexOptions.CultureInvariant);
+        Regex namePattern = RegexConstants.namePattern;
+        // 10 or 12 digits
+        Regex idPattern = RegexConstants.idPattern;
+        Regex salesPattern = RegexConstants.salesPattern;
         
         // Loop until: namePattern is matched
         Console.WriteLine();
@@ -325,9 +439,9 @@ static class Program
         userInput = "";
         
         // Loop until: district logic is fulfilled
-        // Anything not fully numeric
+        // Name of district can be any word or words separated by a space or a hyphen
         Console.WriteLine();
-        while (System.Text.RegularExpressions.Regex.IsMatch(userInput, @"^\d+$") || userInput.Length < 1)
+        while (!namePattern.IsMatch(userInput))
         {
             (int _, int top) = Console.GetCursorPosition();
             Console.SetCursorPosition(0, top - 1);
@@ -352,11 +466,19 @@ static class Program
         return new SalesPerson(name, id, district, sales);
     }
 
-    static void WriteSalesPerson(SalesPerson salesPerson, string path)
+    static void WriteSalesPersonCSV(SalesPerson salesPerson, string path)
     {
         // Append to 'sales_data' file
         using StreamWriter sw = File.AppendText(path);
         sw.WriteLine($"{salesPerson.Name},{salesPerson.Id},{salesPerson.District},{salesPerson.Sales}");
+    }
+    
+    static void WriteSalesPersonLog(SalesPerson salesPerson, string path)
+    {
+        // Append to 'sales_data' file
+        using StreamWriter sw = File.AppendText(path);
+        // Temporarily clutter the log file that the program reads, then prints the real output to the log file
+        sw.WriteLine($"{salesPerson.Name} {salesPerson.Id} {salesPerson.District} {salesPerson.Sales} ");
     }
     
     static void Main(string[] args)
@@ -385,7 +507,7 @@ static class Program
             {
                 case "1":
                     SalesPerson salesPerson = PromptSalesPerson();
-                    WriteSalesPerson(salesPerson, "sales_data");
+                    WriteSalesPersonLog(salesPerson, "sales_data2");
                     break;
                 case "2":
                     mainLoop = false;
